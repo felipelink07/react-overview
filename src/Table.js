45f60eb4186e7600fa4ea8cdc862f653ddd7fb9a
Felipe Felix <felipfelixsantos07@gmail.com>
2020-09-29 21:11:50 -0300

    Envios de arquivos para o repositório

---
+++ src/Table.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react'
+
+//Simple components
+
+const TableHeader = () => {
+    return (
+
+    <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+    </thead>        
+
+    );
+};
+
+
+const TableBody = (props) => {
+
+  const rows = props.characterData.map((row, index) => {
+
+    return (
+
+      <tr key = {index}>
+
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+
+      </tr>
+
+    );
+
+  });
+
+  return <tbody>{rows}</tbody>         
+
+};
+
+//Class Components
+
+class Table extends Component {
+  render() {
+
+    const {characterData} = this.props;
+
+    return (
+      <table>
+
+        <TableHeader />
+        <TableBody  characterData={characterData} />
+
+      </table>
+    )
+  }
+}
+
+export default Table
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Table from './Table';
+
+
+
+ReactDOM.render(<App/>, document.getElementById('root'))
